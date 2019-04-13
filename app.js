a3b600ae1a9a13011ad0536504d1832392709c4f
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-04-12 22:24:31 -0400

    last commit before refactor to react

--- app.js
+++ app.js
@@ -1,145 +1,234 @@
-import NewsAPI from ('./node_modules/newsapi')
-const NewsAPI = Noderequire('newsapi');
-const newsapi = new NewsAPI('ccdad157151647f9a27caa79bcbb7c42');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-});
-
 const app = {};
-let dateAndTime = ['9:30AM', '10:00AM', '10:30AM', '11:00AM', '11:30AM ','12:00PM', '12:30PM', '1:00PM', '1:30PM', '2:00PM', '2:30PM', '3:00PM', '3:30PM', '4:00PM'];
+
+let dateAndTime = [];
 app.key = '5Z8WIM18J4EFOBQS';
 // Pull stocks from API
 app.getStocks = (placeHolderQuery) => {
-$.ajax({
+    $.ajax({
         url: 'https://www.alphavantage.co/query',
         method: 'GET',
         dataType: 'json',
-        data:{
+        data: {
             apikey: app.key,
             function: "SYMBOL_SEARCH",
             keywords: placeHolderQuery
         }
-}).then((result) => {
-    app.displayStocks(result.bestMatches);
-    // console.log(result.bestMatches[0]['1. symbol'])
-});
-
-} // end of getStocks
-
+    }).then((result) => {
+        app.displayStocks(result.bestMatches);
+        // console.log(result.bestMatches[0]['1. symbol'])
+    });
+} 
 app.secondCall = (stock) => {
-$.ajax({
+    $.ajax({
         url: 'https://www.alphavantage.co/query',
         method: 'GET',
         dataType: 'json',
-        data:{
+        data: {
             apikey: app.key,
             function: "TIME_SERIES_INTRADAY",
             interval: '30min',
             symbol: stock,
             outputsize: "compact"
         }
-}).then((result) => {
-    console.log(result);
-    dateAndTime = Object.keys(result['Time Series (30min)'])
-    splicedDateAndTime = dateAndTime.splice(0, 75).reverse()
-    let mappedPrice = splicedDateAndTime.map((value) => {
-        return parseInt(result['Time Series (30min)'][value]['4. close'])
-    })
-    myChart(splicedDateAndTime, mappedPrice);
-});
+    }).then((result) => {
+        dateAndTime = Object.keys(result['Time Series (30min)'])
+        splicedDateAndTime = dateAndTime.splice(0, 75).reverse()
+        let mappedPrice = splicedDateAndTime.map((value) => {
+            return parseInt(result['Time Series (30min)'][value]['4. close'])
+        });
+        app.myChart(splicedDateAndTime, mappedPrice);  
+    });
 }
 // Create eventlistener
-test = function(ticker){
-// console.log(ticker)
-app.secondCall(ticker)
+test = function (ticker) {
+    // console.log(ticker)
+    $('.ticker').empty();
+    app.secondCall(ticker)
+    
+    $('ul').empty();
+    $('.ticker').append(`Ticker: ${ticker}`)
 }
 // Display stocks to screen
 app.displayStocks = (stockList) => {
-$('ul').empty();
-stockList.forEach((stock) => {
- const stockHTML = `
-                    <li onclick="test('${stock['1. symbol']}')" ><span>${stock['2. name']}</span><span> ${stock['1. symbol']}<img class="plusIcon"src="plus-circle-solid.svg"></span></li>
- `
- $('ul').append(stockHTML)
-});
+    $('ul').empty();
+    stockList.forEach((stock) => {
+        const stockHTML = `
+                            <li onclick="test('${stock['1. symbol']}')" ><span>${stock['2. name']}</span><span> ${stock['1. symbol']}<img class="plusIcon"src="plus-circle-solid.svg"></span></li>
+                            `
+        $('ul').append(stockHTML)
+    });
+}
+app.getNews = (newsQuery) => {
+    $.ajax({
+        url: 'https://newsapi.org/v2/everything',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apikey: 'ccdad157151647f9a27caa79bcbb7c42',
+            q: newsQuery,
+            sources: 'bloomberg, business-insider'
+        }
+    }).then((result) => {
+        console.log(result)
+        app.displayNews(result.articles)
+    });
+}
+app.displayNews = (newsList) => {
+    
+    console.log(newsList[0])
+    $('aside').empty();
+    newsList.forEach((newsItem) => {
+        console.log(newsItem)
+        console.log(newsItem.urlToImage)
+        if(newsItem.urlToImage != null){
+            const newsHTML = `
+            <div class="flexCard">
+                <img src=${newsItem.urlToImage}>  
+                <h2>${newsItem.title}</h2>
+            </div>            
+            `
+            $('aside').append(newsHTML)
+        }
+    })
 }
 
-// Initialize
+// Initialize function
 app.init = () => {
-$('form').on('submit', function(e){
-    e.preventDefault();
-    let query = $('#search').val();
-    app.getStocks(query);
-})
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        let query = $('#search').val();
+        app.getStocks(query);
+        app.getNews(query)
+    })
 }
 // Initialize app
-$(function(){
+$(function () {
     app.init();
 });
 
-
-// SCRIPT.JS START
+// Chart JS
 var ctx = document.getElementById('myChart');
-myChart = (splicedDateAndTime, price) => new Chart(ctx, {
+app.myChart = (splicedDateAndTime, price) => new Chart(ctx, {
     type: 'line',
     data: {
         labels: splicedDateAndTime,
         datasets: [{
-            label: 'Price',
+            label: '5 Day Price Chart',
             data: price,
-            // backgroundColor: [
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //     'rgba(255, 206, 86, 0.2)',
-            //     'rgba(75, 192, 192, 0.2)',
-            //     'rgba(153, 102, 255, 0.2)',
-            //     'rgba(255, 159, 64, 0.2)',
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //     'rgba(255, 206, 86, 0.2)',
-            //     'rgba(75, 192, 192, 0.2)',
-            //     'rgba(153, 102, 255, 0.2)',
-            //     'rgba(255, 159, 64, 0.2)',
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //      'rgba(153, 102, 255, 0.2)',
-            //     'rgba(255, 159, 64, 0.2)',
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)'
-            // ],
-            // borderColor: [
-            //     'rgba(255, 99, 132, 1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)',
-            //     'rgba(255, 99, 132, 1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)',
-            //     'rgba(255, 99, 132, 1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)',
-            //     'rgba(255, 99, 132, 1)',
-            //     'rgba(54, 162, 235, 1)'
-            // ],
-            borderWidth: 2
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',               
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',                
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',               
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',               
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+
+            ],
+            borderWidth: 3
         }]
     },
     options: {
@@ -152,4 +241,5 @@ myChart = (splicedDateAndTime, price) => new Chart(ctx, {
         }
     }
 });
-// 
+
+// End of Chart JS
